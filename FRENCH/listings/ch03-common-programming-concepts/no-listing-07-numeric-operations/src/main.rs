526249730e12ec4959e532851a85e648617b65ba
simon cardona <simon.cdna@proton.me>
2025-11-09 08:36:20 +0100

    fix(exemple): ch03-02

--- FRENCH/listings/ch03-common-programming-concepts/no-listing-07-numeric-operations/src/main.rs
+++ FRENCH/listings/ch03-common-programming-concepts/no-listing-07-numeric-operations/src/main.rs
@@ -1,17 +1,17 @@
 fn main() {
     // addition
-    let somme = 5 + 10;
+    let sum = 5 + 10;
 
     // soustraction
     let difference = 95.5 - 4.3;
 
     // multiplication
-    let produit = 4 * 30;
+    let product = 4 * 30;
 
     // division
     let quotient = 56.7 / 32.2;
-    let arrondi = 2 / 3; // retournera 0
+    let floored = 2 / 3; // retournera 0
 
     // modulo
-    let reste = 43 % 5;
+    let remainder = 43 % 5;
 }
--- FRENCH/listings/ch03-common-programming-concepts/no-listing-09-char/src/main.rs
+++ FRENCH/listings/ch03-common-programming-concepts/no-listing-09-char/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     let c = 'z';
     let z = 'ℤ';
-    let chat_aux_yeux_de_coeur = '😻';
+    let heart_eyed_cat = '😻';
 }
